165c82e7001a0f210b82858f04ad3e158653a23f
trdxlucifer <singhdepu566@gmail.com>
2023-08-30 20:15:11 +0530

    created itsm function

--- src/App.jsx
+++ src/App.jsx
@@ -65,3 +65,15 @@ function Stats() {
 }
 
 
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {""}
+        {item.quantity}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
