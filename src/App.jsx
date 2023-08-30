9681e0f2a24ff05b9785f5d30f86055cc563e672
trdxlucifer <singhdepu566@gmail.com>
2023-08-30 20:13:49 +0530

    created logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 
-const a = () => {
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Charger", quantity: 12, packed: false },
+];
+
+import React from "react";
+
+const App = () => {
   return (
-    <div>a</div>
-  )
-}
+    <div className="app">
+      <Logo />
+      {/* <Form />
+      <PackingList />
+      <Stats /> */}
+    </div>
+  );
+};
 
-export default a
+export default App;
+
+function Logo() {
+  return <h1> 🏝️ Far Away 🧳</h1>;
+}
