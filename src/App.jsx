9deb858f9aa9b6b5c135d9770ca278bf8bcc5a65
trdxlucifer <singhdepu566@gmail.com>
2023-08-31 17:55:24 +0530

    created a tggle item to say packed or unpacked

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,33 @@ import React, { useState } from "react";
 
 const App = () => {
   const [item, setItems] = useState([]);
+
   function handleItem(item) {
     setItems((items) => [...items, item]);
   }
+
+  function handleDeleteItem(id) {
+   
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleItem} />
-      <PackingList items={item} />
+      <PackingList
+        items={item}
+        onDeleteItems={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
       <Stats />
     </div>
   );
@@ -78,16 +97,12 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItems,onToggleItem }) {
   return (
     <div className="list">
-       <ul>
+      <ul>
         {items.map((item) => (
-          <Item
-            item={item}
-            
-            key={item.id}
-          />
+          <Item item={item} onDeleteItems={onDeleteItems} key={item.id} onToggleItem={onToggleItem}  />
         ))}
       </ul>
     </div>
@@ -102,15 +117,20 @@ function Stats() {
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItems, onToggleItem }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {""}
         {item.quantity}
         {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItems(item.id)}>❌</button>
     </li>
   );
 }
